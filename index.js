0fa8773a7ea1bac1dd40cce101359db4b55faac8
ZonaBR <71289282+ZonaBRDev@users.noreply.github.com>
2021-10-24 21:59:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+import request from 'superagent';
+import * as config from './config';
+import lodash from 'lodash';
+import $ from 'jquery';
+
+import {
+    expect
+} from 'chai';
+
+let originalMemberLength;
+let runTimes = 0;
+let token;
+
+
+const logger = (text) => {
+    var div = document.createElement('div');
+    div.innerHTML = text;
+    document.body.appendChild(div);
+}
